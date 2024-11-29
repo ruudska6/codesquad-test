574cea1f7c3702279210a441f47cffdfb96b78a2
ruudska6 <feels19@naver.com>
2024-11-29 20:11:43 +0900

    feat: InputView 사용자의 입력을 받기 위한 getUserInput() 메서드 작성

---
+++ src/lotto/InputView.java
@@ -0,0 +1,29 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+    private static final String USER_INPUT_MESSAGE = "1~45 중 로또 번호를 여섯개 입력하세요";
+    List<Integer> userInputs = new ArrayList<>();
+
+    public void getUserInput() {
+        System.out.println(USER_INPUT_MESSAGE);
+        Scanner scanner = new Scanner(System.in);
+
+        for(int i = 0; i < 6; i++) {
+            int userInput = scanner.nextInt();
+            if(isDuplicate(userInput)) {
+                System.out.println("같은 번호" + userInput + "이 이미 선택되었습니다. 다른 번호를 선택하세요");
+            } else {
+                userInputs.add(userInput);
+            }
+        }
+        scanner.close();
+    }
+
+    private boolean isDuplicate(int number) {
+        return userInputs.contains(number);
+    }
+}
